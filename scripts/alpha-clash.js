54552f4f45da60b50d7a88be8e5fac87dc17dffb
maab-fw <maabfw@proton.me>
2024-02-14 22:01:19 +0600

    almost done(with 2 way) without scoreborad

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -6,9 +6,56 @@
 //     playGround.classList.remove('hidden')
 // }
 
+function handleKeyboardButtonPress(event) {
+    const playerPressed = event.key;
+    // console.log('player pressed', playerPressed);
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('currentAlphabet')
+    const currentAlphabet = currentAlphabetElement.innerText
+    const expectedAlphabet = currentAlphabet.toLowerCase()
+    // console.log(expectedAlphabet, playerPressed);
+
+    if (playerPressed === expectedAlphabet) {
+        console.log('you got one point');
+
+        const currentScore = getTextElementValueById('currentScore')
+        const updateScore = currentScore + 1
+        setTextElementValueById('currentScore', updateScore)
+        // ---------------------------------------------------------
+        // const currentScoreElement = document.getElementById('currentScore');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText)
+
+
+        // const newScore = currentScore + 1
+        // currentScoreElement.innerText = newScore
+
+        removeBGColorById(expectedAlphabet)
+        continueGame()
+    }
+    else {
+        console.log('you lose');
+
+        const currentLife = getTextElementValueById('currentLife')
+        const updateLife = currentLife - 1
+        setTextElementValueById('currentLife', updateLife)
+        // const currentLifeElement = document.getElementById('currentLife')
+        // const currentLifeText = currentLifeElement.innerText
+        // const currentLife = parseInt(currentLifeText)
+
+        // const newLife = currentLife - 1
+        // currentLifeElement.innerText = newLife
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardButtonPress);
+
 function continueGame() {
     // step 1: generate a random alphabet
     const alphabet = getARandomAlphabet()
+    // console.log('your random alphabet', alphabet);
+
     const currentAlphabet = document.getElementById('currentAlphabet')
     currentAlphabet.innerText = alphabet
 
--- scripts/utility.js
+++ scripts/utility.js
@@ -18,6 +18,18 @@ function removeBGColorById(elementId) {
     element.classList.remove('bg-[gold]')
 }
 
+function getTextElementValueById(elementId) {
+    const element = document.getElementById(elementId)
+    const elementValueText = element.innerText
+    const value = parseInt(elementValueText)
+    return value;
+}
+
+function setTextElementValueById(elementId, value) {
+    const element = document.getElementById(elementId)
+    element.innerText = value
+}
+
 function getARandomAlphabet() {
     // get or create an alphabet array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
